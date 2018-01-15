edb1fca727fdf0f493f382e28f10f560e7e24759
Johannes Borgström <johannes.borgstrom@tretton37.com>
2018-01-15 10:59:16 +0100

    Add cms middleware

---
+++ src/cms/index.js
@@ -0,0 +1,14 @@
+/**
+ * CMS
+ * 
+ * This is an API for the Content Managing System. It is meant as a middleware for accessing 
+ * content while we decide what technical solution we're gonna use.
+ */
+
+ class cms {
+   constructor() {
+     console.log('CMS Inited')
+   }
+ }
+
+ export default cms
