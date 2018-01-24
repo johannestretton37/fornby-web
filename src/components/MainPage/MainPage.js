1dce1959cbc2053e6412adc8420f656d85d8da49
Johannes Borgström <johannes.borgstrom@tretton37.com>
2018-01-24 11:44:40 +0100

    Fetch available pages from cms

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,18 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import GalleryPage from '../GalleryPage'
-import ContactPage from '../ContactPage'
-import { ContentGroup } from '../../constants'
+import Loading from '../Loading'
 import ErrorPage from '../ErrorPage'
+import cms from '../../cms'
 import './MainPage.css'
 
-const MainPage = ({ match }) => {
-  // TODO: - Load from cms
-  const pages = {
-    [ContentGroup.COURSES]: <GalleryPage contentType={ContentGroup.COURSES} title="Kurser" />,
-    kontakt: <ContactPage />,
+class MainPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoading: true,
+      pages: {}
+    }
   }
-  const page = match.params.page
-  return pages[page] || <ErrorPage />
+
+  componentDidMount() {
+    this.loadPages()
+  }
+
+  loadPages = async () => {
+    let mainMenuItems = await cms.mainMenuItems()
+    let pages = {}
+    mainMenuItems.forEach(item => {
+      const contentGroupName = item.url.replace('/', '')
+      pages[contentGroupName] = <GalleryPage contentType={contentGroupName} title={item.title} />
+    })
+    this.setState({
+      pages,
+      isLoading : false
+    })
+  }
+  render() {
+    let {isLoading, pages} = this.state
+    const page = this.props.match.params.page
+    return (
+      isLoading ?
+        <Loading />
+      : 
+        pages[page] || <ErrorPage />
+    )
+  }
+}
+
+MainPage.propTypes = {
+  match: PropTypes.object.isRequired
 }
 
 export default MainPage
