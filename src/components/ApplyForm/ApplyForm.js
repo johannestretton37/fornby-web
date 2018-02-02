274b074b8b48a84eb7d51c9f46332c54a554c0eb
Johannes Borgström <johannes.borgstrom@tretton37.com>
2018-02-02 16:27:20 +0100

    Fade in schoolsoft iframe once it is loaded

--- src/components/ApplyForm/ApplyForm.js
+++ src/components/ApplyForm/ApplyForm.js
@@ -3,12 +3,30 @@ import { Row, Col } from 'reactstrap'
 import './ApplyForm.css'
   
 class ApplyForm extends Component {
+  state = {
+    iframeSrc: '',
+    isLoaded: false
+  }
+
+  componentDidMount() {
+    this.setState({
+      iframeSrc: 'https://sms.schoolsoft.se/fhsk/jsp/LoginApplicant.jsp#/'
+    })
+  }
+
+  handleLoad = () => {
+    this.setState({
+      isLoaded: true
+    })
+  }
+
   render() {
+    const { iframeSrc, isLoaded } = this.state
     return (
       <Row>
         <Col>
-          <div className='iframe-container'>
-            <iframe id='schoolsoft' title='Ansökan SchoolSoft' src='https://sms.schoolsoft.se/fhsk/jsp/LoginApplicant.jsp#/'></iframe>
+          <div className='iframe-container' style={{ backgroundColor: isLoaded ? 'rgba(255,255,255,0)' : '#eee' }}>
+            <iframe id='schoolsoft' style={{ opacity: isLoaded ? 1 : 0 }} onLoad={this.handleLoad} title='Ansökan SchoolSoft' src={iframeSrc}></iframe>
           </div>
         </Col>
       </Row>
