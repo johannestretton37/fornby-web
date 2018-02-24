aafd01bb2bb5304081db09164ea611bcfa55ed96
Jesper Turesson <Jturesson90@gmail.com>
2018-02-24 15:30:55 +0100

    Submenu in "Kurser"

--- src/components/SubMenu/SubMenu.js
+++ src/components/SubMenu/SubMenu.js
@@ -4,6 +4,7 @@ import { Nav, NavItem, Col } from 'reactstrap'
 import { object } from 'prop-types'
 import cms from '../../cms'
 import './SubMenu.css'
+import { PageSlug, ContentGroup } from '../../constants'
 
 class SubMenu extends Component {
   state = {
@@ -15,34 +16,72 @@ class SubMenu extends Component {
   }
 
   componentDidMount() {
+    console.log(`Submenu for ${this.props.match.params.page}`)
+    console.log(this.props.match)
     this.getSubMenu(this.props.match.params.page)
+
   }
 
+  getCoursesMenuItems(pageContent) {
+    let menuItems = []
+    if (this.props.match.params.subpage === undefined || !pageContent.courseCategories) return this.menuItems;
+    const page = '/' + this.props.match.params.page
+    pageContent.courseCategories.forEach(subpage => {
+      let menuItem = {
+        title: subpage.name,
+        url: page + '/' + subpage.slug
+      }
+      if (subpage.courses) {
+        subpage.courses.forEach(course => {
+          let { name, slug } = course
+          if (!menuItem.subItems) menuItem.subItems = []
+          menuItem.subItems.push({
+            title: name,
+            url: page + '/' + subpage.slug + '/' + slug
+          })
+        })
+      }
+      menuItems.push(menuItem)
+    })
+    return menuItems;
+  }
+  getSubPagesMenuItems(pageContent) {
+    let menuItems = []
+    const page = '/' + this.props.match.params.page
+    pageContent.subPages.forEach(subPage => {
+      let menuItem = {
+        title: subPage.name,
+        url: page + '/' + subPage.slug
+      }
+      if (subPage.detailPages) {
+        subPage.detailPages.forEach(detailPage => {
+          let { name, slug } = detailPage.detailPage[0]
+          if (!menuItem.subItems) menuItem.subItems = []
+          menuItem.subItems.push({
+            title: name,
+            url: page + '/' + subPage.slug + '#' + slug
+          })
+        })
+      }
+      menuItems.push(menuItem)
+    })
+    return menuItems;
+  }
   getSubMenu = (page) => {
     return new Promise(async resolve => {
       const pageContent = await cms.getPageContent(page)
       let menuItems = []
-      if (pageContent.subPages) {
-        const page = '/' + this.props.match.params.page
-        pageContent.subPages.forEach(subPage => {
-          let menuItem = {
-            title: subPage.name,
-            url: page + '/' + subPage.slug
-          }
-          if (subPage.detailPages) {
-            subPage.detailPages.forEach(detailPage => {
-              let { name, slug } = detailPage.detailPage[0]
-              if (!menuItem.subItems) menuItem.subItems = []
-              menuItem.subItems.push({
-                title: name,
-                url: page + '/' + subPage.slug + '#' + slug
-              })
-            })
-          }
-          menuItems.push(menuItem)
-        })
+
+      if (page === PageSlug.COURSES) {
+        menuItems = this.getCoursesMenuItems(pageContent);
+      } else if (pageContent.subPages) {
+        menuItems = this.getSubPagesMenuItems(pageContent);
       }
+
+
       this.setState({ menuItems })
+      console.log(`Done with getting submenus: ${menuItems}`);
+      console.log({ menuItems });
       resolve()
     })
   }
@@ -74,13 +113,13 @@ class SubMenu extends Component {
     const { menuItems } = this.state
     return (
       menuItems && menuItems.length > 0 ?
-      <Col md="3" className='sub-menu-container'>
-        <Nav className='sub-menu' vertical>
-          {menuItems.map(this.menuItem)}
-        </Nav>
-      </Col>
-      :
-      null
+        <Col md="3" className='sub-menu-container'>
+          <Nav className='sub-menu' vertical>
+            {menuItems.map(this.menuItem)}
+          </Nav>
+        </Col>
+        :
+        null
     )
   }
 }
